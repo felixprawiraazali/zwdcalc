dcb89ede676582bb9538a8f5b2aed614deb96f3f
Felix Prawira Azali <felixprawira8@gmail.com>
2025-05-20 13:25:53 +0700

    setup for offline access

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'zwds-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/iztro.js',
+  './js/stars_mapping.js',
+  './js/transformation.js',
+  './js/tailwind.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
